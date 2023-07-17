43bb0d4240a073934b8a53b42069913268169b90
anthonyrivelli <139507843+anthonyrivelli@users.noreply.github.com>
2023-07-17 17:28:41 +0200

    Add files via upload
    Modifiche funzionali

--- filosofi.c
+++ filosofi.c
@@ -2,72 +2,69 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define NUM_FILOSOFI 5
+#define NUMERO_FILOSOFI 5
 
-pthread_t filosofi[NUM_FILOSOFI];
-int haPresoLaForchetta[NUM_FILOSOFI];
+pthread_t filosofi[NUMERO_FILOSOFI];
+int haPresoLaForchetta[NUMERO_FILOSOFI];
 int pos;
 
-void schermoIniziale();
-void inizializza(int arrayDiBool[NUM_FILOSOFI]);
-void stampaStato(int pos, char* messaggio);
-void *prendiForchetta(void* numeroForchette);
-void creaThread(pthread_t filosofi[NUM_FILOSOFI],int* numeroForchette);
+void schermoMenu();
+void inizializzaArray(int arrayDiBool[NUMERO_FILOSOFI]);
+void stampaStato(int pos,  char* messaggio);
+void* prendiForchetta(void* numeroForchette);
+void creaThread(pthread_t filosofi[NUMERO_FILOSOFI], int* numeroForchette);
 void signal(int* s);
 void wait(int* s);
-void inizializza(int haMangiato[NUM_FILOSOFI]);
+void inizializzaArray(int haMangiato[NUMERO_FILOSOFI]);
 
-int main(){
-    int numeroForchette = NUM_FILOSOFI;
-    schermoIniziale();
-    inizializza(haPresoLaForchetta);
-    int continua = 1;
-    while(1){
-        creaThread(filosofi,&numeroForchette);
+void schermoMenu() {
+    printf("+-------------------------BENVENUTO--------------------------+\n");
+    printf("|Applicazione per gestire il problema dei filosofi a cena.|\n");
+    printf("+------------------------------------------------------------+\n");
+}
+
+int main() {
+    int numeroForchette = NUMERO_FILOSOFI;
+    schermoMenu();
+    inizializzaArray(haPresoLaForchetta);
+    while (1) {
+        creaThread(filosofi, &numeroForchette);
     }
     return 0;
 }
 
-void inizializza(int arrayDiBool[NUM_FILOSOFI]){
-    for(int i = 0; i < NUM_FILOSOFI; i++){
+void inizializzaArray(int arrayDiBool[NUMERO_FILOSOFI]) {
+    for (int i = 0; i < NUMERO_FILOSOFI; i++) {
         arrayDiBool[i] = 0;
     }
 }
 
-
-void *prendiForchetta(void* numeroForchette){
-    int n;
+void* prendiForchetta(void* numeroForchette) {
     int aspetta, segnale;
-    n = NUM_FILOSOFI/2;
-    pthread_join((pthread_t)filosofi[pos],0);
-    
-    if(*(int*)numeroForchette >= n && haPresoLaForchetta[pos] == 0){
-        
-        wait(*(int*)numeroForchette);
+    int num = NUMERO_FILOSOFI / 2;
+    pthread_join(filosofi[pos], NULL);
+    if (*(int*)numeroForchette >= num && !haPresoLaForchetta[pos]) {
+        wait((int*)numeroForchette);
         haPresoLaForchetta[pos] = 1;
         stampaStato(pos, "HA PRESO LA FORCHETTA");
         printf("Numero forchette: %d\n", *(int*)numeroForchette);
-        pthread_join(filosofi[pos],0);
-        pthread_exit(0);
-        
-    } else if(haPresoLaForchetta[pos] == 1 && *(int*)numeroForchette < n){
-        stampaStato(pos,"STA MANGIANDO");
+        pthread_join(filosofi[pos], NULL);
+        pthread_exit(NULL);
+    } else if (haPresoLaForchetta[pos] && *(int*)numeroForchette < num) {
+        stampaStato(pos, "STA MANGIANDO");
         haPresoLaForchetta[pos] = 0;
-        
-        signal(*(int*)numeroForchette);
+        signal((int*)numeroForchette);
         printf("Numero forchette: %d\n", *(int*)numeroForchette);
-        pthread_exit(0);
-    } else if(haPresoLaForchetta[pos] == 0 && *(int*)numeroForchette < n){
-        stampaStato(pos,"STA PENSANDO");
-        pthread_exit(0);
+        pthread_exit(NULL);
+    } else if (!haPresoLaForchetta[pos] && *(int*)numeroForchette < num) {
+        stampaStato(pos, "STA PENSANDO");
+        pthread_exit(NULL);
     }
-    
-    pthread_exit(0);
+    pthread_exit(NULL);
 }
 
-void creaThread(pthread_t filosofi[NUM_FILOSOFI], int* numeroForchette) {
-    int pos;
-    for (pos = 0; pos < NUM_FILOSOFI; pos++) {
+void creaThread(pthread_t filosofi[NUMERO_FILOSOFI], int* numeroForchette) {
+    for (pos = 0; pos < NUMERO_FILOSOFI; pos++) {
         usleep(0.00001);
         pthread_create(&filosofi[pos], NULL, prendiForchetta, (void*)numeroForchette);
         pthread_join(filosofi[pos], NULL);
@@ -75,8 +72,9 @@ void creaThread(pthread_t filosofi[NUM_FILOSOFI], int* numeroForchette) {
 }
 
 void wait(int* s) {
-    while (*s < 0)
+    while (*s < 0) {
         pthread_exit(NULL);
+    }
     (*s)--;
 }
 
@@ -84,12 +82,8 @@ void signal(int* s) {
     (*s)++;
 }
 
-void stampaStato(int pos, char* messaggio) {
+void stampaStato(int pos,  char* messaggio) {
     printf("FILOSOFO: %d %s\n", pos + 1, messaggio);
 }
 
-void schermoIniziale() {
-    printf("+-------------------------BENVENUTO--------------------------+\n");
-    printf("|Quest'applicazione gestisce il problema dei cinque filosofi.|\n");
-    printf("+------------------------------------------------------------+\n");
-}
+
